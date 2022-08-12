e78dab87b6a8a35493906e683e80d47f58775fc7
mcbreard <daniel.m@indigomed.com>
2022-08-12 14:29:04 +0200

    cleanup

--- cobs_test.c
+++ cobs_test.c
@@ -8,27 +8,13 @@
 #include <string.h>
 #include "cobs.h"
 
+#define MARKER_BYTE 0xAB
+#define MAX_TEST_SIZE 260	
+
 #define ASSERT_EQUAL_LUINT(value, expected) \
     do {\
         if( (value) != (expected) ) { \
             printf( "%30s: Failed, %s != %s. Expected %lu, got %lu\n", __func__, #value, #expected, (unsigned long)(expected), (unsigned long)(value) ); \
-            return false; \
-        } \
-    } while(0)
-
-
-#define ASSERT_LTE_LUINT(value, expected) \
-    do {\
-        if( !( (value) <= (expected) ) ) { \
-            printf( "%30s: Failed, %s > %s. Expected <= %lu, got %lu\n", __func__, #value, #expected, (unsigned long)(expected), (unsigned long)(value) ); \
-            return false; \
-        } \
-    } while(0)
-
-#define ASSERT_EQUAL_UINT8_HEX(value, expected) \
-    do {\
-        if( (value) != (expected) ) { \
-            printf( "%30s: Failed, %s != %s. Expected 0x%02X, got 0x%02X\n", __func__, #value, #expected, (expected), (value) ); \
             return false; \
         } \
     } while(0)
@@ -41,45 +27,7 @@
                 return false; \
             } \
         } \
-   } while(0)
-
-#define ROUNDTRIP_TEST_RUNNER( input, length ) \
-    do { \
-        size_t working_buffer_length = (length) + (length) / 254 + 2; \
-        uint8_t *working_buffer = malloc(working_buffer_length); \
-        uint8_t *decoded_buffer = malloc(length + 1); \
-        memset( working_buffer, MARKER_BYTE, working_buffer_length ); \
-        decoded_buffer[length] = MARKER_BYTE; \
-\
-        size_t encoded_length = cobs_encode( input, length, working_buffer ); \
-        ASSERT_LTE_LUINT(encoded_length, length + (length)/254 + 1); \
-        ASSERT_EQUAL_UINT8_HEX( working_buffer[encoded_length], MARKER_BYTE ); \
-        ASSERT_EQUAL_UINT8_HEX( working_buffer[working_buffer_length - 1], MARKER_BYTE ); \
-\
-        size_t decoded_length = cobs_decode( working_buffer, encoded_length, decoded_buffer ); \
-\
-/*      printf("Original:" ); \
-        for( size_t i = 0; i < length; i++ ) printf(" %02X", (input)[i] ); \
-        printf("\nEncoded:" ); \
-        for( size_t i = 0; i < working_buffer_length; i++ ) printf(" %02X", working_buffer[i] ); \
-        printf("\nDecoded:" ); \
-        for( size_t i = 0; i < length; i++ ) printf(" %02X", decoded_buffer[i] ); \
-        printf("\n" ); */\
-        ASSERT_EQUAL_LUINT( decoded_length, length ); \
-        ASSERT_EQUAL_MEM( decoded_buffer, input, length ); \
-        ASSERT_EQUAL_UINT8_HEX( decoded_buffer[length], MARKER_BYTE ); \
-\
-        free(working_buffer); /* Not run if the tests fail, oh well */ \
-        free(decoded_buffer); \
-\
-        return true; \
-    } while(0)
-
-#define MARKER_BYTE 0xAB
-#define MAX_TEST_SIZE 260		
-
-static unsigned int test_count = 0;
-static uint8_t working_buffer[MAX_TEST_SIZE];
+   } while(0)	
 
 #define SETUP_TEST 	\
 	do {	\
@@ -114,6 +62,9 @@ static uint8_t working_buffer[MAX_TEST_SIZE];
 			return false; \
 		}	\
 	}
+	
+static unsigned int test_count = 0;
+static uint8_t working_buffer[MAX_TEST_SIZE];
 
 // Wikipedia Example 1
 bool test_single_null( void )
@@ -304,156 +255,6 @@ bool test_cobs_encode_254_bytes_trailing_null_one(void) // Wikipedia Example 11
 	return true;
 }
 
-/*
-
-bool test_255_bytes_null_end( void )
-{
-	SETUP_TEST;
-    uint8_t buffer[255];
-    for( unsigned int i = 0; i < sizeof(buffer) - 1; i++ )
-    {
-        buffer[i] = i + 1;
-    }
-    buffer[sizeof(buffer) - 1] = 0;
-
-    uint8_t working_buffer[257];
-    uint8_t expected_buffer[257];
-
-    expected_buffer[0] = 0xFF;
-    for( int i = 0; i < 254; i++ )
-    {
-        expected_buffer[i + 1] = i + 1;
-    }
-    expected_buffer[256] = 1;
-    expected_buffer[257] = MARKER_BYTE;
-
-    working_buffer[256] = MARKER_BYTE;
-
-    size_t encoded_length = cobs_encode( buffer, sizeof(buffer), working_buffer );
-
-    ASSERT_EQUAL_MEM( working_buffer, expected_buffer, sizeof(working_buffer) );
-    ASSERT_EQUAL_LUINT( encoded_length, 256 );
-
-    return true;
-}
-
-bool test_hex1_rt( void )
-{
-	SETUP_TEST;
-    uint8_t buffer[] = { 1 };
-
-    ROUNDTRIP_TEST_RUNNER( buffer, sizeof(buffer) );
-}
-
-bool test_single_null_rt( void )
-{
-	SETUP_TEST;
-    uint8_t buffer[] = { 0 };
-
-    ROUNDTRIP_TEST_RUNNER( buffer, sizeof(buffer) );
-}
-
-bool test_two_nulls_rt( void )
-{
-	SETUP_TEST;
-    uint8_t buffer[] = { 0, 0 };
-
-    ROUNDTRIP_TEST_RUNNER( buffer, sizeof(buffer) );
-}
-
-bool test_null_one_null_rt( void )
-{
-	SETUP_TEST;
-    uint8_t buffer[] = { 0, 1, 0 };
-
-    ROUNDTRIP_TEST_RUNNER( buffer, sizeof(buffer) );
-}
-
-bool test_null_two_null_one_rt( void )
-{
-	SETUP_TEST;
-    uint8_t buffer[] = { 0, 2, 0, 1 };
-
-    ROUNDTRIP_TEST_RUNNER( buffer, sizeof(buffer) );
-}
-
-bool test_254_bytes_rt( void )
-{
-	SETUP_TEST;
-    uint8_t buffer[254];
-    for( unsigned int i = 0; i < sizeof(buffer); i++ )
-    {
-        buffer[i] = i + 1;
-    }
-
-    ROUNDTRIP_TEST_RUNNER( buffer, sizeof(buffer) );
-}
-
-bool test_254_bytes_null_end_rt( void )
-{
-	SETUP_TEST;
-    uint8_t buffer[254];
-    for( unsigned int i = 0; i < sizeof(buffer) - 1; i++ )
-    {
-        buffer[i] = i + 1;
-    }
-    buffer[sizeof(buffer) - 1] = 0;
-
-    ROUNDTRIP_TEST_RUNNER( buffer, sizeof(buffer) );
-}
-
-bool test_255_bytes_rt( void )
-{
-	SETUP_TEST;
-    uint8_t buffer[255];
-    for( unsigned int i = 0; i < sizeof(buffer); i++ )
-    {
-        buffer[i] = i % 254 + 1;
-    }
-
-    ROUNDTRIP_TEST_RUNNER( buffer, sizeof(buffer) );
-}
-
-bool test_255_bytes_null_end_rt( void )
-{
-	SETUP_TEST;
-    uint8_t buffer[255];
-    for( unsigned int i = 0; i < sizeof(buffer) - 1; i++ )
-    {
-        buffer[i] = i + 1;
-    }
-    buffer[sizeof(buffer) - 1] = 0;
-
-    ROUNDTRIP_TEST_RUNNER( buffer, sizeof(buffer) );
-}
-
-bool test_256_bytes_rt( void )
-{
-	SETUP_TEST;
-    uint8_t buffer[256];
-    for( unsigned int i = 0; i < sizeof(buffer); i++ )
-    {
-        buffer[i] = i % 254 + 1;
-    }
-
-    ROUNDTRIP_TEST_RUNNER( buffer, sizeof(buffer) );
-}
-
-bool test_256_bytes_null_end_rt( void )
-{
-	SETUP_TEST;
-    uint8_t buffer[256];
-    for( unsigned int i = 0; i < sizeof(buffer) - 1; i++ )
-    {
-        buffer[i] = i + 1;
-    }
-    buffer[sizeof(buffer) - 1] = 0;
-
-    ROUNDTRIP_TEST_RUNNER( buffer, sizeof(buffer) );
-}
-
-*/
-
 int main(int argc, char*argv[])
 {
     test_single_null();
@@ -468,20 +269,6 @@ int main(int argc, char*argv[])
 	test_cobs_encode_255_bytes_no_null();
 	test_cobs_encode_254_bytes_trailing_null();
 	test_cobs_encode_254_bytes_trailing_null_one();
-
-/*
-    test_hex1_rt();
-    test_single_null_rt();
-    test_two_nulls_rt();
-    test_null_one_null_rt();
-    test_null_two_null_one_rt();
-    test_254_bytes_rt();
-    test_254_bytes_null_end_rt();
-    test_255_bytes_rt();
-    test_255_bytes_null_end_rt();
-    test_256_bytes_rt();
-    test_256_bytes_null_end_rt();
-	*/
 	
 	printf("ran %d COBS unit tests\n", test_count);
 
